5cdca662a029f78bdc2df55ab8f4d3bf9d67c142
Sanket Shigaonkar <sanket.shigaonkar211@gmail.com>
2025-12-26 03:01:18 -0500

    Time: 19 ms (8.61%), Space: 17.8 MB (89.33%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # # FIrst thing that came to my mind
+        answer = ""
+        for i in range(len(s)):
+            if s[i].isalnum():
+                answer += s[i].lower()
+        
+        # reverse_answer = answer[::-1]
+
+        # return answer == reverse_answer
+
+        #trying using two pointers
+        left, right = 0, len(s)-1
+
+        while(left<=right):
+            if not s[left].isalnum():
+                left +=1
+                continue
+
+            if not s[right].isalnum():
+                right -=1
+                continue
+          
+            if(s[left].lower()==s[right].lower()):
+                left+=1
+                right-=1
+            else:
+                return False
+
+        return True
+
+
+
+                

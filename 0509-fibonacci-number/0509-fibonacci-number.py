1fc49e495752029b3136ba4da588ea94a4cbd757
Sanket Shigaonkar <sanket.shigaonkar211@gmail.com>
2026-01-05 15:15:37 -0500

    Time: 394 ms (5.02%), Space: 17.5 MB (61.25%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,19 +1,27 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n==0:
-            return 0
+        def recursion(num):
+            if num==0:
+                return 0
+            if num==1:
+                return 1
 
-        elif n==1 or n==2:
-            return 1
+            return recursion(num-1)+recursion(num-2)
 
-        else:
-            num1 = 1
-            num2 = 1
-            for _ in range(3,n+1):
-                res = num1 + num2
-                num1 = num2
-                num2 = res
+        return recursion(n)
+        # if n==0:
+        #     return 0
 
-            return res  
+        # elif n==1 or n==2:
+        #     return 1
 
+        # else:
+        #     num1 = 1
+        #     num2 = 1
+        #     for _ in range(3,n+1):
+        #         res = num1 + num2
+        #         num1 = num2
+        #         num2 = res
+
+        #     return res  
                 
